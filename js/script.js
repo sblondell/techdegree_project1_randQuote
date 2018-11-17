388a3a18e44a76b11120573297c5af556089953a
Stephan Blondell <stephan.blondell@gmail.com>
2018-11-17 15:24:33 -0500

    added year property to array, fixed functions per instructions

--- js/script.js
+++ js/script.js
@@ -20,52 +20,62 @@ var quotes = [
   {
     quote : 'Always do right. This will gratify some people and astonish the rest.',
     source : 'Mark Twain',
-    citation : "Note to the Young People's Society, Greenpoint Presbyterian Church, 1901"
+    citation : "Note to the Young People's Society, Greenpoint Presbyterian Church"
+    year : '1901'
   },
   {
     quote : 'If you are going through hell, keep going.',
     source : 'Winston Churchill',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'Sometimes the road less traveled is less traveled for a reason',
     source : 'Jerry Seinfeld',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'Just remember, once you\'re over the hill you begin to pick up speed.',
     source : 'Arthur Schopenhauer',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'All my life, I always wanted to be somebody. Now I see that I should have been more specific.',
     source : 'Jane Wagner',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'Friendship is born at that moment when one person says to another: What! You too? I thought I was the only one.',
     source : 'C.S. Lewis',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'Your work is to discover your world and then with all your heart give yourself to it.',
     source : 'Buddha',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'True friends are families which you can select.',
     source : 'Audrey Hepburn',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'The more I give to thee, the more I have, for both are infinite.',
     source : 'William Shakespeare',
-    citation : ''
+    citation : '',
+    year : ''
   },
   {
     quote : 'I used to think anyone doing anything weird was weird. Now I know that it is the people that call others weird that are weird.',
     source : 'Paul McCartney',
-    citation : ''
+    citation : '',
+    year : ''
   }
 ];
 
@@ -79,12 +89,12 @@ var quotes = [
      `quotes` array.
 ***/
 
-function getRandomQuote(){
+function getRandomQuote(array){
   var randNum = 0;
 
-  randNum = Math.floor(Math.random() * quotes.length);
+  randNum = Math.floor(Math.random() * array.length);
 
-  return quotes[randNum];
+  return array[randNum];
 }
 
 
@@ -100,20 +110,22 @@ function getRandomQuote(){
 ***/
 
 function printQuote(){
-  var randQuote = getRandomQuote();
+  var randQuote = getRandomQuote(quotes);
   var outputDiv = document.getElementById('quote-box');
   var html = '';
 
   html += "<p class='quote'>" + randQuote.quote + '</p>';
   html += "<p class='source'>" + randQuote.source;
   if (randQuote.citation){
-    html += "<span class=citation>" + randQuote.citation + "</span>";
+    html += "<span class='citation'>" + randQuote.citation + "</span>";
+  }
+  if (randQuote.year){
+    html += "<span class='citation'>" + randQuote.year + "</span>";
   }
   html += '</p>';
 
   outputDiv.innerHTML = html;
 }
-
 
 
 /***
